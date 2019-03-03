752b29eacb23ce78a2210bd193e4eb7ce68d3d17
rakshit <rakshit@192.168.0.6>
2019-03-03 18:43:23 +0530

    First Commit
    Uploaded all files

---
+++ my-library/src/main/java/com/sample/mylibrary/MyLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.sample.mylibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLibraryApplication.class, args);
+	}
+
+}
